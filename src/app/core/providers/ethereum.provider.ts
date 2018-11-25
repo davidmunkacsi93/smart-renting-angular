a583a4d3fde0f318d84f9b26e5743d20bcca5e92
David Munkacsi <david.munkacsi@campus.tu-berlin.de>
2018-11-25 15:03:10 +0100

    Creating connection between Frontend and Ethereum network WIP.

---
+++ src/app/core/providers/ethereum.provider.ts
@@ -0,0 +1,8 @@
+import { InjectionToken } from '@angular/core';
+import Web3  from 'web3';
+import { Provider } from 'web3/providers';
+
+export const EthereumProvider = new InjectionToken<Web3>('web3', {
+  providedIn: 'root',
+  factory: () => new Web3(new Provider())
+});
