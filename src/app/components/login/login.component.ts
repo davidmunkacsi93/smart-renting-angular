e3e188924a6cffe408923a697b62c81df0cccd44
David Munkacsi <david.munkacsi@campus.tu-berlin.de>
2018-11-30 19:09:18 +0100

    Building the form WIP. For some reason it is not displayed.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core'
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+
+import { AuthenticationService } from 'src/app/core/services/authentication.service';
 
 @Component({
     selector: 'app-login',
@@ -6,11 +9,29 @@ import { Component, OnInit, OnDestroy } from '@angular/core'
     styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit, OnDestroy {
-    ngOnInit() {
 
+    loginForm: FormGroup;
+    submitted: boolean = false;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private authenticationService: AuthenticationService
+    ) {
+
+    }
+    ngOnInit() {
+        this.loginForm = this.formBuilder.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required]
+          });
     }
 
     ngOnDestroy() {
-        
+
+    }
+
+    onSubmitForm() {
+        console.log("Successful.")
+        // Authenticate via Ethereum.
     }
 }
