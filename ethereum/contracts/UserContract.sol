e61d61a72588e1d017a0be0704d11fc677a4b6ca
David Munkacsi <david.munkacsi@campus.tu-berlin.de>
2018-12-01 19:18:24 +0100

    UserContract semantically correct.

--- ethereum/contracts/UserContract.sol
+++ ethereum/contracts/UserContract.sol
@@ -7,25 +7,29 @@ contract UserContract {
         string username;
         bytes32 password;
         address userAddress;
+        bool isExisting;
     }
 
     mapping (string => User) userMapping;
 
     function createUser(string memory username, string memory password) public {
-        User memory user = User(username, keccak256(abi.encode(password)), msg.sender);
+        User memory user = User(username, keccak256(abi.encode(password)), msg.sender, true);
         userMapping[username] = user;
     }
 
     function authenticate(string memory username, string memory password)
-        public view returns (string memory _username, address _userAddress) {
+        public view returns (bool _success, string memory _username, address _userAddress) {
 
         if (userMapping[username].password == keccak256(abi.encode(password))) {
             User storage user = userMapping[username];
-            return (user.username, user.userAddress);
+            return (true, user.username, user.userAddress);
         }
+
+        return (false, "", address(0));
     }
 
-    function validateUsername(string memory _username) public returns (bool) {
+    function isUsernameExisting(string memory username) public view returns (bool) {
+        if (userMapping[username].isExisting) return false;
         return true;
     }
 }
