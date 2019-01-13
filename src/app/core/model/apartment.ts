408e0e04b6138f25198e2503c01e3be266b8de1c
David Munkacsi <david.munkacsi@campus.tu-berlin.de>
2019-01-13 15:18:15 +0100

    Apartment model added.

---
+++ src/app/core/model/apartment.ts
@@ -0,0 +1,14 @@
+export class User {
+    Id: number;
+    Owner: string;
+    Tenant: string;
+    PostCode: number;
+    City: string;
+    Street: string;
+    HouseNumber: number;
+    Floor: number;
+    Description: string;
+    Rent: number;
+    Deposit: number;
+    IsRented: boolean;
+}
